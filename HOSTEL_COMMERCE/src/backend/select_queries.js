6534a57cd6f57608b6490558e7a6b1688c48992b
Suraj <surajkrishna004@gmail.com>
2024-09-19 00:30:38 +0530

    Student Table added

---
+++ HOSTEL_COMMERCE/src/backend/select_queries.js
@@ -0,0 +1,23 @@
+import supabase from "./supabaseclient.js";
+const Student_Display=async(args)=> {
+  let student, error;
+
+  if (args.length === 1 && args[0] === "*") {
+    ({ data: student, error } = await supabase.from("student").select("*"));
+  } else if (args.length === 1) {
+    ({ data: student, error } = await supabase
+      .from("student")
+      .select(args[0]));
+  } else if (args.length > 1) {
+    ({ data: student, error } = await supabase
+      .from("student")
+      .select(args.join(",")));
+  }
+
+  if (error) {
+    console.error("Error fetching data:", error);
+  } else {
+    console.log(student);
+  }
+}
+export default {Student_Display}
---
+++ HOSTEL_COMMERCE/src/backend/test.js
@@ -0,0 +1,3 @@
+import select_queries from "./select_queries.js";
+const data=["*"]
+await select_queries.Student_Display(data);
