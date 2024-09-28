a452cf965eb0d9d2db50d650c7278da2dc291bfe
Suraj <surajkrishna004@gmail.com>
2024-09-28 11:57:19 +0530

    added owner,hostel,vendor models

---
+++ HOSTEL_COMMERCE/src/backend/Vendor/model.js
@@ -0,0 +1,15 @@
+class Vendor{
+    constructor(vendorid,vendorname,verified,address,city,pincode,state)
+    {
+        this.vendorid=vendorid;
+        this.vendorname=vendorname;
+        this.verified=verified;
+        this.address=address;
+        this.city=city;
+        this.pincode=pincode;
+        this.state=state;
+    }
+}
+
+
+export default Vendor;
