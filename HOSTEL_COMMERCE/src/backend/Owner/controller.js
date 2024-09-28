12d750ec72749ca14a0a479237a67f2d6113b094
Suraj <surajkrishna004@gmail.com>
2024-09-28 12:29:32 +0530

    add hostel

--- HOSTEL_COMMERCE/src/backend/Owner/controller.js
+++ HOSTEL_COMMERCE/src/backend/Owner/controller.js
@@ -1,5 +1,6 @@
 import supabase from "../util/supabaseclient.js";
 import Owner from "./model.js";
+import Hostel from "../Hostel/model.js";
 
 //The form data from the owner end is send as an json object to the following method 
 //hostel_details should be a key-value pair data structure
@@ -21,37 +22,28 @@ import Owner from "./model.js";
     constraint hostel_pkey primary key (hostelid),
     constraint hostel_ownerid_fkey foreign key (ownerid) references owner (ownerid)
   ) tablespace pg_default; */
-async function AddHostel(ownerid,hostel_details)
+async function AddHostel(ownerid,hostel)
 {
 
 
     const { data, error } = await supabase
     .from('hostel')
     .insert([
-      {  hostelid:"100",
-        hostelname:"check", 
-        ownerid:"5",
-        addressline1:"asdas", 
-        addressline2:"asdasd",  
-        city:"assad" , 
-        pincode:"12312" ,
-        state :"asdsd",
-        capacity :"03",
-        type:"mens",
-        contactnumber:"9567456123" ,
-        rating :"9",
-        verification:"true" },
+      {  hostelid:hostel.hostelid,
+        hostelname:hostel.hostelname, 
+        ownerid:hostel.owner,
+        addressline1:hostel.addressline1, 
+        addressline2:hostel.addressline2,  
+        city:hostel.city, 
+        pincode:hostel.pincode,
+        state :hostel.state,
+        capacity :hostel.capacity,
+        type:hostel.type,
+        contactnumber:hostel.contactnumber ,
+        rating :hostel.rating,
+        verification:hostel.verification},
     ])
             
         
 }
-const test={};
-try {
-  
-  const ownerTest=new Owner(101,'a','b','c','d/d/d','12312312312');
-  console.log(ownerTest.id);
-}
-catch(Exception)
-{
-     console.log(Exception);
-}
+
