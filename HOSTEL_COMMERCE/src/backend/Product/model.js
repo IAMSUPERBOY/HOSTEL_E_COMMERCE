01754c232c031f3f2379b789b0ea16b56bde6ba6
Suraj12P <surajpmnr@gmail.com>
2024-09-28 12:03:52 +0530

    product and student model created

---
+++ HOSTEL_COMMERCE/src/backend/Product/model.js
@@ -0,0 +1,14 @@
+class Product
+{
+    constructor(productid,productname,category,price,id)
+    {
+        this.productid=productid;
+        this.productname=productname;
+        this.category=category;
+        this.price=price;
+        this.id=id;
+        
+    }
+}
+
+export default Product;
---
+++ HOSTEL_COMMERCE/src/backend/Student/model.js
@@ -0,0 +1,25 @@
+class Student
+{
+    constructor(studentid,fname,lname,instname,yearofstudy,age,gender,guardianname,phno,addressline1,addressline2,city,pincode,state,currentresidence,hostelid)
+    {
+        this.studentid=studentid;
+        this.fname=fname;
+        this.lname=lname;
+        this.instname=instname;
+        this.yearofstudy=yearofstudy;
+        this.age=age;
+        this.gender = gender;
+        this.guardianname = guardianname;
+        this.phno = phno;
+        this.addressline1 = addressline1;
+        this.addressline2 = addressline2;
+        this.city  = city;
+        this.pincode = pincode;
+        this.state = state;
+        this.currentresidence = currentresidence;
+        this.hostelid = hostelid;
+        
+    }
+}
+
+export default Student;
