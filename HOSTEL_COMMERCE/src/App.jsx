271e24e74371ae945b595be627945e08ce7e2f81
Suraj12P <surajpmnr@gmail.com>
2024-10-09 14:20:41 +0530

    ownerregistration added

--- HOSTEL_COMMERCE/src/App.jsx
+++ HOSTEL_COMMERCE/src/App.jsx
@@ -3,13 +3,14 @@ import AddNewHostel from "./views/Owner/AddNewHostel";
 import EditHostel from "./views/Owner/EditHostel"
 import AddNewRoom from "./views/Owner/AddNewRoom";
 import RequestPage from "./views/Owner/Request";
+import OwnerRegisration from "./views/Owner/OwnerRegistration";
 
 import "./App.css";
 
 function App() {
   return( 
   <>
-  <AddNewRoom />
+  <OwnerRegisration />
  </>
   
 
